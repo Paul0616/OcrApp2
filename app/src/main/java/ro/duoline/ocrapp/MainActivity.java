77745bae67bff5c28a19c2bcfc593a251e2c2085
Paul <omd.paul@gmail.com>
2017-10-15 17:39:50 +0300

    modificat

--- app/src/main/java/ro/duoline/ocrapp/MainActivity.java
+++ app/src/main/java/ro/duoline/ocrapp/MainActivity.java
@@ -29,6 +29,8 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.text.Text;
@@ -44,7 +46,11 @@ public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_WRITE_PERMISSION = 20;
     private static final int PHOTO_REQUEST = 10;
 
-    private static final String ORAS_CAUTAT = "FOCSANI";
+    private static final String[] ORAS_CAUTAT = new String[]{"FOCSANI", "BUC.", "PREDEAL"};
+    private Boolean[] oraseGasite = new Boolean[]{false,false,false};
+    private String dataDetectata;
+    private String oraDetectata;
+    private String pretTotal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
         poza = (ImageView) findViewById(R.id.imageView);
         scanResults = (TextView) findViewById(R.id.results);
         requestStoragePermission();
+        //oraseGasite = new ArrayList<Boolean>();
 
         detector = new TextRecognizer.Builder(getApplicationContext()).build();
 
@@ -135,70 +142,49 @@ public class MainActivity extends AppCompatActivity {
                                 //extract scanned text blocks here
                                 TextBlock tBlock = textBlocks.valueAt(index);
                                 blocks = blocks + tBlock.getValue() + "\n" + "\n";
+
                                 for (Text line : tBlock.getComponents()) {
                                     //extract scanned text lines here
                                     lines = lines + line.getValue() + "\n";
+
                                     for (Text element : line.getComponents()) {
                                         //extract scanned text words here
-                                        words = words + element.getValue() + "#" + "\n";
+                                        words = words + element.getValue() + "#";//+ "\n"
                                     }
                                 }
                             }
                             if (textBlocks.size() == 0) {
                                 scanResults.setText("Nu pot detecta text in imaginea scanata");
                             } else {
-                              //  scanResults.setText(scanResults.getText() + "Blocks: " + "\n");
-                              //  scanResults.setText(scanResults.getText() + blocks + "\n");
-                              //  scanResults.setText(scanResults.getText() + "---------" + "\n");
-                             //   scanResults.setText(scanResults.getText() + "Lines: " + "\n");
-                              //  scanResults.setText(scanResults.getText() + lines + "\n");
-                                scanResults.setText(scanResults.getText() + "Lista de cuvinte detectate asemanatoare:" + "\n");
-                              //  scanResults.setText(scanResults.getText() + "Words: " + "\n");
-                              //  scanResults.setText(scanResults.getText() + words + "\n");
-                                List<String> myList = new ArrayList<String>(Arrays.asList(words.split("#")));
-                                List<Float> floatList = new ArrayList<Float>();
-                                Float f;
-                                String tempMyListelement;
-                                Boolean orasulString = false;
-                                for(int i = 0; i < myList.size(); i++) {
-                                    if(myList.get(i).toString().toUpperCase().contains(ORAS_CAUTAT)) {
-                                        orasulString = true;
-                                        break;
-                                    }
-                                    int z = compareStrings(myList.get(i).toString().toUpperCase().replaceAll("\\s",""), ORAS_CAUTAT);
-                                    if (z > 0 && z < 5 && myList.get(i).toString().toUpperCase().replaceAll("\\s","").length() > 4){
-                                        scanResults.setText(scanResults.getText() + myList.get(i).toString().toUpperCase()+ " --- " + Integer.valueOf(z) + "\n");
-                                    }
-                                    if(z == 1){
-                                        orasulString = true;
-                                        break;
-                                    }
-                                }
-                                for(int i = 0; i < myList.size(); i++){
-                                    try {
-                                        tempMyListelement = myList.get(i).toString().replace(',','.');
-                                        tempMyListelement = tempMyListelement.replace("RON","0");
-                                        f = Float.parseFloat(tempMyListelement);
-                                        if (tempMyListelement.contains(".") && tempMyListelement.charAt(tempMyListelement.length() -1) != '.') {
-                                            floatList.add(f);
-                                        }
+                                scanResults.setText(scanResults.getText() + "Blocks: " + "\n");
+                                scanResults.setText(scanResults.getText() + blocks + "\n");
+                                scanResults.setText(scanResults.getText() + "---------" + "\n");
+                                scanResults.setText(scanResults.getText() + "Lines: " + "\n");
+                                scanResults.setText(scanResults.getText() + lines + "\n");
+                                scanResults.setText(scanResults.getText() + "---------" + "\n");
 
-                                    } catch (NumberFormatException nfe){
-                                        nfe.printStackTrace();
-                                    }
+                                scanResults.setText(scanResults.getText() + "Words: " + "\n");
+                                scanResults.setText(scanResults.getText() + words + "\n");
+                                scanResults.setText(scanResults.getText() + "---------" + "\n");
+                              //  for(int i = 0; i<ORAS_CAUTAT.length; i++){
+                               //     oraseGasite.add(false);
+                              //  }
+                                detectezOras(words);
+                                dataDetectata = "";
+                                oraDetectata = "";
+                                detectezDataOra(words);
+                                String afisaj = "Orasul: ";
+                                for(int i = 0; i<oraseGasite.length; i++){
+                                    afisaj = afisaj + (oraseGasite[i] ? (ORAS_CAUTAT[i] + "\n") : "");
+                                }
+                                afisaj = afisaj + "Data: " + dataDetectata + "\n";
+                                afisaj = afisaj + "Ora: " + oraDetectata + "\n";
 
-                                }
-                                if(floatList.size() > 0) {
-                                    float pretTotal = floatList.get(0);
-                                    scanResults.setText(scanResults.getText() + "Lista de numere cu zecimale detectate:");
-                                    for(Float fl: floatList){
-                                        if(fl > pretTotal) pretTotal = fl;
-                                        scanResults.setText(scanResults.getText() + "\n" + fl.toString());
-                                    }
-                                    if(orasulString) scanResults.setText(scanResults.getText() + "\n" + ORAS_CAUTAT + " " + "\n");
-                                    scanResults.setText(scanResults.getText() + "\n" + "Total: " + Float.toString(pretTotal));
-                                }
-                             //   scanResults.setText(scanResults.getText() + "---------" + "\n");
+                                afisaj = afisaj + "Total: " + detectezTotal(words);
+                                scanResults.setText(afisaj);
+                                //Toast.makeText(getBaseContext(),afisaj, Toast.LENGTH_LONG).show();
+
+
                             }
                         } else {
                             scanResults.setText("Nu se poate porni modulul OCR!");
@@ -212,6 +198,102 @@ public class MainActivity extends AppCompatActivity {
             } // ACTION_TAKE_PHOTO_B
 
         }
+    }
+
+    private String detectezTotal(String words){
+        //pretTotal ="";
+        String pt = "";
+        List<String> myList = new ArrayList<String>(Arrays.asList(words.split("#")));
+        String tempMyListelement;
+        //pretTotal = 0.00;
+
+        List<Float> floatList = new ArrayList<Float>();
+
+        for(int i = 0; i < myList.size(); i++){
+            try {
+                tempMyListelement = myList.get(i).toString().replace(',','.');
+                tempMyListelement = tempMyListelement.replace("RON","");
+                if(tempMyListelement.matches("[0-9]+(\\.[0-9][0-9]?)")){
+                    floatList.add(Float.parseFloat(tempMyListelement));
+                }
+             //  f = Float.parseFloat(tempMyListelement);
+            //    if (tempMyListelement.contains(".") && tempMyListelement.charAt(tempMyListelement.length() -1) != '.') {
+             //       floatList.add(f);
+            //    }
+
+            } catch (NumberFormatException nfe){
+                nfe.printStackTrace();
+            }
+
+        }
+        if(floatList.size() > 0) {
+            float pretTotal = floatList.get(0);
+            for(Float fl: floatList){
+                if(fl > pretTotal) {
+                    pt = Float.toString(fl);
+                }
+            }
+
+
+        }
+        return pt;
+
+    }
+    private void detectezOras(String words){
+        List<String> myList = new ArrayList<String>(Arrays.asList(words.split("#")));
+       // iesire:
+
+        for(int i = 0; i<myList.size();i++) {
+            for (int j = 0; j < ORAS_CAUTAT.length; j++) {
+                if (sirAcceptabil(myList.get(i).toString(), ORAS_CAUTAT[j])){
+                    //Toast.makeText(getBaseContext(),"Am detectat orasul: " + ORAS_CAUTAT[j], Toast.LENGTH_LONG).show();
+                    //break iesire;
+                    oraseGasite[j] = true;
+                }
+            }
+        }
+
+    }
+    public void detectezDataOra(String words){
+        List<String> myList = new ArrayList<String>(Arrays.asList(words.split("#")));
+        for(int i = 0; i<myList.size();i++) {
+            if (myList.get(i).matches("(0[1-9]|1[0-9]|2[0-9]|3[01]).(0[1-9]|1[012]).[0-9]{4}")){
+                dataDetectata = myList.get(i).toString();
+            }
+        }
+        for(int i = 0; i<myList.size();i++) {
+            if (myList.get(i).matches("(0[1-9]|1[0-9]|2[0-3])(:[0-5][0-9])")){
+                oraDetectata = myList.get(i).toString();
+            }
+        }
+        for(int i = 0; i<myList.size();i++) {
+            if (myList.get(i).matches("(0[0-9]|1[0-9]|2[0-3])(:[0-5][0-9]){2}")){
+                oraDetectata = myList.get(i).toString();
+            }
+        }
+
+    }
+
+    private Boolean sirAcceptabil(String sirDetectat, String orasEtalon){ //reintoarce true daca diferenta dintre siruri e acceptabila si se considera potrivire si false daca diferenta e prea mare
+        int lungimeDetectat = sirDetectat.length();
+        int lungimeEtalon = orasEtalon.length();
+        int diferentaDeLungime = Integer.max(lungimeDetectat, lungimeEtalon) - Integer.min(lungimeDetectat,lungimeEtalon);
+        if(diferentaDeLungime > 1){
+            return false; //diferenta de lungime intre siruri mai mare decat 1: potrivire inacceptabila
+        } else {
+            int z = compareStrings(sirDetectat.toUpperCase().replaceAll("\\s",""), orasEtalon); //numarul de litere care difera intre cele 2 cuvinte
+            if(z > 2) {
+                return false;
+            }
+            else {
+                if(lungimeDetectat > 3) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+
     }
 
     private Bitmap decodeBitmapUri(Context ctx, Uri uri) throws FileNotFoundException {
